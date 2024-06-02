4f6cbae26a0733aab629ef82d378dd9c84a2070b
Atul <atultiwariee60@gmail.com>
2024-06-03 02:01:32 +0530

    heaps implementation

--- heaps_implementation/max_pq_implementation.cpp
+++ heaps_implementation/max_pq_implementation.cpp
@@ -1,76 +1,115 @@
-#include<bits/stdc++.h>
+#include <vector>
+#include <iostream>
 
+using namespace std;
 
-class PriorityQueue {
-    // Complete this class
-    vector<int>data;
-    public:
-   bool isEmpty(){
-       return data.size()==0;
-   }
-   int getSize(){
-       return data.size();
-   } 
-   int getMax(){
-       if(isEmpty()){ // isEmpty() called on current object of priorityQueue class 
-           return INT_MIN;
-       }
-       return data[0];
-   } 
-    
-   void insert(int element){
-       data.push_back(element);
-       
-       //up-heapify
-       int childIndex=data.size()-1;
-       while(childIndex > 0){
-           int parentIndex=(childIndex-1)/2;
-               if(data[parentIndex] < data[childIndex]){
-                   swap(data[parentIndex],data[childIndex]);
-               }
-               else{ //inserted at correct position
-                   break;
-               }
-           childIndex=parentIndex;
-       }
-   }
-   
-    int removeMax(){
-         if(isEmpty()) {
-            return 0;		// Priority Queue is empty
+class PriorityQueue
+{
+    vector<int> data;
+
+public:
+    bool isEmpty()
+    {
+        return data.size() == 0;
+    }
+
+    int getSize()
+    {
+        return data.size();
+    }
+
+    int getMax()
+    {
+        if (isEmpty())
+        { // isEmpty() called on current object of priorityQueue class
+            return INT_MIN;
         }
-        
-        int ans=data[0];
-        data[0]=data[data.size()-1];
-        data[data.size()-1]=ans;
-        data.pop_back();
-        
-        //DOWN-HEAPIFY  
-        int parentIndex=0;
-        int leftChildIndex  = 2*(parentIndex)+1;
-        int rightChildIndex = 2*(parentIndex)+2;
-        
-        while(leftChildIndex<data.size()){
-            int maxIndex = parentIndex;
-            if(data[maxIndex]<data[leftChildIndex]){
-                maxIndex=leftChildIndex;
+        return data[0];
+    }
+
+    void insert(int element)
+    {
+        data.push_back(element);
+
+        // up-heapify
+        int childIndex = data.size() - 1;
+        while (childIndex > 0)
+        {
+            int parentIndex = (childIndex - 1) / 2;
+            if (data[parentIndex] < data[childIndex])
+            {
+                swap(data[parentIndex], data[childIndex]);
             }
-            if(rightChildIndex<data.size() && data[maxIndex]<data[rightChildIndex]){
-                maxIndex=rightChildIndex;
-            }
-            
-            if(maxIndex==parentIndex){
+            else
+            { // inserted at correct position
                 break;
             }
-            else{
-                swap(data[maxIndex],data[parentIndex]);
+            childIndex = parentIndex;
+        }
+    }
+
+    int removeMax()
+    {
+        if (isEmpty())
+        {
+            return INT_MIN; // Priority Queue is empty
+        }
+
+        int ans = data[0];
+        data[0] = data[data.size() - 1];
+        data[data.size() - 1] = ans;
+        data.pop_back();
+
+        // DOWN-HEAPIFY
+        int parentIndex = 0;
+        int leftChildIndex = 2 * (parentIndex) + 1;
+        int rightChildIndex = 2 * (parentIndex) + 2;
+
+        while (leftChildIndex < data.size())
+        {
+            int maxIndex = parentIndex;
+            if (data[maxIndex] < data[leftChildIndex])
+            {
+                maxIndex = leftChildIndex;
             }
-            parentIndex=maxIndex;
-            leftChildIndex  = 2*(parentIndex)+1;
-            rightChildIndex = 2*(parentIndex)+2;
+            if (rightChildIndex < data.size() && data[maxIndex] < data[rightChildIndex])
+            {
+                maxIndex = rightChildIndex;
+            }
+
+            if (maxIndex == parentIndex)
+            {
+                break;
+            }
+            else
+            {
+                swap(data[maxIndex], data[parentIndex]);
+            }
+            parentIndex = maxIndex;
+            leftChildIndex = 2 * (parentIndex) + 1;
+            rightChildIndex = 2 * (parentIndex) + 2;
         }
         return ans;
     }
-         
-         
 };
+
+
+int main()
+{
+    PriorityQueue pq;
+    int data;
+    cout<<"Enter data to be inserted in max PQ"<<endl;
+    cin>>data;
+
+    while(data!=-1){
+        pq.insert(data);
+        cout<<"Enter data to be inserted in max PQ"<<endl;
+        cin>>data;
+    }
+
+    cout<<pq.getMax()<<endl;
+
+    pq.removeMax();
+    cout<<pq.getMax()<<endl;
+
+}
